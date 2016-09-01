143ec5c051c547244bac2af121e7ab7436e78447
Sven Straubinger <straubinger.es@googlemail.com>
2016-09-01 12:14:27 +0200

    Add custom table view cell

---
+++ zaragoza/ZAStopTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ZAStopTableViewCell.h
+//  zaragoza
+//
+//  Created by Sven Straubinger on 01/09/16.
+//  Copyright © 2016 Sven Straubinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZAStopTableViewCell : UITableViewCell
+
+@end
---
+++ zaragoza/ZAStopTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ZAStopTableViewCell.m
+//  zaragoza
+//
+//  Created by Sven Straubinger on 01/09/16.
+//  Copyright © 2016 Sven Straubinger. All rights reserved.
+//
+
+#import "ZAStopTableViewCell.h"
+
+@implementation ZAStopTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
