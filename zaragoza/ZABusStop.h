23f0ed9262f0abf75c5e0a26b8ccfa76b88650ec
Sven Straubinger <straubinger.es@googlemail.com>
2016-09-01 18:02:05 +0200

    Add bus stop model

---
+++ zaragoza/ZABusStop.h
@@ -0,0 +1,13 @@
+//
+//  ZABusStop.h
+//  zaragoza
+//
+//  Created by Sven Straubinger on 01/09/16.
+//  Copyright © 2016 Sven Straubinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZABusStop : NSObject
+
+@end
---
+++ zaragoza/ZABusStop.m
@@ -0,0 +1,13 @@
+//
+//  ZABusStop.m
+//  zaragoza
+//
+//  Created by Sven Straubinger on 01/09/16.
+//  Copyright © 2016 Sven Straubinger. All rights reserved.
+//
+
+#import "ZABusStop.h"
+
+@implementation ZABusStop
+
+@end
