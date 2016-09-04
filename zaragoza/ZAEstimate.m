c2f020a580980c73d40ad47e849013c4ed3841df
Sven Straubinger <straubinger.es@googlemail.com>
2016-09-04 22:10:46 +0200

    Add estimate object

---
+++ zaragoza/ZAEstimate.m
@@ -0,0 +1,20 @@
+//
+//  ZAEstimate.m
+//  zaragoza
+//
+//  Created by Sven Straubinger on 04/09/16.
+//  Copyright © 2016 Sven Straubinger. All rights reserved.
+//
+
+#import "ZAEstimate.h"
+
+@implementation ZAEstimate
+
++(EKObjectMapping *)objectMapping
+{
+    return [EKObjectMapping mappingForClass:self withBlock:^(EKObjectMapping *mapping) {
+        [mapping mapPropertiesFromArray:@[@"line", @"direction", @"estimate"]];
+    }];
+}
+
+@end
