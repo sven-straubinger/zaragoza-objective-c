b89d54b8e6ec967ad813a0b8a967f8017a057244
Sven Straubinger <straubinger.es@googlemail.com>
2016-09-04 18:30:43 +0200

    Update model

--- zaragoza/ZABusStop.h
+++ zaragoza/ZABusStop.h
@@ -8,11 +8,14 @@
 
 #import <Foundation/Foundation.h>
 #import <EasyMapping.h>
+#import <UIKit/UIKit.h>
 
 @interface ZABusStop : NSObject <EKMappingProtocol>
 
-@property (nonatomic) NSString *identifier;
-@property (nonatomic) NSString *name;
-@property (nonatomic) NSString *eta;
+@property (nonatomic, strong) NSString *identifier;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *eta;
+@property (nonatomic, strong) UIImage *image;
+@property (nonatomic, strong) NSString *imageUrl;
 
 @end
