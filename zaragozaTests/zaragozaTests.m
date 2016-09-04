456c8c97af6fd134d449419ab5bcaff6680f2b17
Sven Straubinger <straubinger.es@googlemail.com>
2016-09-04 16:13:07 +0200

    Setup view controller tests

--- zaragozaTests/zaragozaTests.m
+++ zaragozaTests/zaragozaTests.m
@@ -10,17 +10,22 @@
 
 @interface zaragozaTests : XCTestCase
 
+@property(nonatomic, strong) UIViewController *viewController;
+
 @end
 
 @implementation zaragozaTests
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // Load view controller from storyboard
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    self.viewController = [storyboard instantiateViewControllerWithIdentifier:@"ZAOverviewViewController"];
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.viewController = nil;
     [super tearDown];
 }
 
@@ -29,6 +34,7 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
+#warning Review performance test
 - (void)testPerformanceExample {
     // This is an example of a performance test case.
     [self measureBlock:^{
